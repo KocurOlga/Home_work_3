380b27e88f5b99d1961ab27b5d168b7f91b96564
Kocur Olga <olkocur@gmail.com>
2020-10-23 14:50:55 +0700

    sort with direction

--- def_bubble_method.py
+++ def_bubble_method.py
@@ -1,10 +1,18 @@
-def sort_list(my_list):
-    for i in range(len(my_list)):
-        for j in range(i, len(my_list)):
-            if my_list[i] > my_list[j]:
-                temp = my_list[i]
-                my_list[i] = my_list[j]
-                my_list[j] = temp
+def sort_list(my_list, direction): #направление сортировки, если 1, то по возрастанию
+    if direction == 1:
+        for i in range(len(my_list)):
+            for j in range(i, len(my_list)):
+                if my_list[i] > my_list[j]:
+                    temp = my_list[i]
+                    my_list[i] = my_list[j]
+                    my_list[j] = temp
+    else:
+        for i in range(len(my_list)):
+            for j in range(i, len(my_list)):
+                if my_list[i] < my_list[j]:
+                    temp = my_list[i]
+                    my_list[i] = my_list[j]
+                    my_list[j] = temp
     return my_list
 
 
@@ -14,8 +22,10 @@ list3 = [7, 2, 4, 1, -1 , 7, 3, -5, 0]
 list4 = [2, -5, 6, 1, 0 , -1, -2, -3, 9]
 list5 = [9, 0, 1, 2, 9 , 3, -1, 4, 2]
 
-print(sort_list(list1))
-print(sort_list(list2))
-print(sort_list(list3))
-print(sort_list(list4))
-print(sort_list(list5))
+direct = int(input("Введите направление сортировки (1 - сортировка по возрастанию): "))
+
+print(sort_list(list1, direct))
+print(sort_list(list2, direct))
+print(sort_list(list3, direct))
+print(sort_list(list4, direct))
+print(sort_list(list5, direct))
